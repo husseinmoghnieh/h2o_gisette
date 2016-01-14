f9b0375e6496e0609860033cf820a8b7652af540
huss <hussein.moghnieh@gmail.com>
2016-01-14 12:44:32 -0800

    fixing hive script

--- generate_model.sh
+++ generate_model.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 Rscript script.R
-sed -i -e '1ipackage ai.h2o.hive.udf\' ./tmp/GBMModel.java
+os_name=uname
+if (${os_name} == 'Linux')
+    sed -i -e '1ipackage ai.h2o.hive.udf\' ./tmp/GBMModel.java
+endif
 cp ./tmp/*.java ./src/main/java/ai/h2o/hive/udf/
 cp ./tmp/*.jar ./localjars/
 cp ./tmp/*.csv ./src/main/resources/
-
-
