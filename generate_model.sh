ba8aeeac099bdb08b0427fe6b48aeccc546122ea
huss <hussein.moghnieh@gmail.com>
2016-01-14 12:48:50 -0800

    modifying scrit to work on linux and mac

--- generate_model.sh
+++ generate_model.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 Rscript script.R
-os_name=uname
-if (${os_name} == 'Linux')
-    sed -i -e '1ipackage ai.h2o.hive.udf\' ./tmp/GBMModel.java
-endif
+sed -i -e '1ipackage ai.h2o.hive.udf\' ./tmp/GBMModel.java
 cp ./tmp/*.java ./src/main/java/ai/h2o/hive/udf/
 cp ./tmp/*.jar ./localjars/
 cp ./tmp/*.csv ./src/main/resources/
--- test.sh
+++ test.sh
@@ -2,8 +2,4 @@
 os_name=sudo uname
 echo ${os_name}
 if %os_name% == "Linux"
-then
-    sed -i -e '1ipackage ai.h2o.hive.udf\' ./tmp/GBMModel.java
-else
-    echo "please set package in GBMModel manually"
-fi
+    echo ${os_name}
