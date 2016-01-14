7b208159651f5b56ad2a30dc4d223d5a3f484f4f
huss <hussein.moghnieh@gmail.com>
2016-01-13 16:30:13 -0800

    fix

--- setup_data_to_score.sh
+++ setup_data_to_score.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-hadoop_url = "ch1.citation.io"
+hadoop_url="ch1.citation.io"
 hadoop fs -rm -r hdfs://${hadoop_url}:/user/root/GisetteScoreTest
 hadoop fs -put ./data/gisette_train.data  hdfs://${hadoop_url}:/user/root/GisetteScoreTest/.
