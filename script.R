50df69bfdb8ea34b6f45aae804d16adc778ca5f6
huss <hussein.moghnieh@gmail.com>
2016-01-22 20:14:04 -0800

    fixing R script

--- script.R
+++ script.R
@@ -1,17 +1,17 @@
 library(h2o)
-#h2o.init()
-localH2O <- h2o.init(ip = "ch2-slave-2.citation.io", port = 54321, startH2O = FALSE)
-train_all  <- h2o.importFile(path = "hdfs://ch2-master.citation.io/user/root/GisetteScoreTestCSV/gisette_train_clean.csv")
+
+localH2O <- h2o.init(ip = "ch3.citation.io", port = 54321, startH2O = FALSE)
+train_all  <- h2o.importFile(path = "hdfs://ch3.citation.io/user/root/GisetteScoreTestCSV/gisette_train_clean.csv")
 preset <- names(train_all)
 
 
-train_label  <- h2o.importFile(path = "hdfs://ch2-master.citation.io/user/root/GisetteScoreTestLabels/gisette_train.labels")
+train_label  <- h2o.importFile(path = "hdfs://ch3.citation.io/user/root/GisetteScoreTestLabels/gisette_train.labels")
 names(train_label)[names(train_label)=="C1"] <- "CLASSIFICATION"
 train_all$CLASSIFICATION <- train_label;
 train_all$CLASSIFICATION = as.numeric(train_all$CLASSIFICATION)
 train_all$CLASSIFICATION = as.factor(train_all$CLASSIFICATION)
 
-#
+# NOT NEEDED FOR THE TIME BEING
 rand_vec <- h2o.runif(train_all, seed = 1234)
 train <- train_all[rand_vec <= 0.8,]
 valid <- train_all[(rand_vec > 0.8),]
@@ -41,31 +41,3 @@ pred_test_2 = h2o.predict(gisette_model, test2)
 
 print(pred_test_1)
 print(pred_test_2)
-
-#h2o.exportFile(test1, "hdfs://ch2-master.citation.io/user/root/tmp/pred_test_1.csv", force = TRUE)
-#h2o.exportFile(test2, "hdfs://ch2-master.citation.io/user/root/tmp/pred_test_2.csv", force = TRUE)
-#
-
-
-#
-## Make and export predictions.
-#h2o_single_test_1  <- h2o.importFile(path = "data/prostate_test_actual0.csv")
-#h2o_single_test_2  <- h2o.importFile(path = "data/prostate_test_actual1.csv")
-#
-#pred_test_1 = h2o.predict(binomial.fit, h2o_single_test_1)
-#pred_test_2 = h2o.predict(binomial.fit, h2o_single_test_2)
-#
-#if (! file.exists("tmp")) {
-#  dir.create("tmp")
-#}
-#
-#h2o.exportFile(pred_test_1, "tmp/pred_test_1.csv", force = TRUE)
-#h2o.exportFile(pred_test_2, "tmp/pred_test_2.csv", force = TRUE)
-#h2o.download_pojo(binomial.fit , path = "tmp")
-#
-## Or you can export the predictions to hdfs:
-##   h2o.exportFile(pred, "hdfs://namenode/path/to/file.csv")1718
-#
-## Calculate metrics.
-##perf = h2o.performance(binomial.fit, test)
-##print(perf)
