af9366ff814d7f201787711be41bf317390651b9
huss <hussein.moghnieh@gmail.com>
2016-02-02 16:56:37 -0500

    Misc fixes

--- run_pojo.sh
+++ run_pojo.sh
@@ -2,4 +2,4 @@
 mvn clean
 mvn install:install-file -Dfile=./localjars/h2o-genmodel.jar -DgroupId=water -DartifactId=h2o-genmodel -Dversion=3.0 -Dpackaging=jar
 mvn package
-mvn exec:java -Dexec.mainClass="pojo.PredictMain"
+mvn exec:java -Dexec.mainClass="ai.h2o.pojo.PredictMain"
